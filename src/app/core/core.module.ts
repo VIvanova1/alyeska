49ea287aa50bf53d898294c7f799cedb1b6116b7
Veneta Ivanova <i.veneta.v@gmail.com>
2023-07-16 16:00:14 +0300

    add footer and about us page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    NavigationComponent
+    NavigationComponent, FooterComponent
   ],
 
 })
