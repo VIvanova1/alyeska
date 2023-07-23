cc8ff5292658165a68a8128ec2dfdc7ec2e5766d
Veneta Ivanova <i.veneta.v@gmail.com>
2023-07-23 13:46:23 +0300

    implement firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyA38hAWVOsylfemAQK2xXcYFqN30Jz5mzQ',
+    authDomain: 'alyeska-fashion.firebaseapp.com',
+    projectId: 'alyeska-fashion',
+    storageBucket: 'alyeska-fashion.appspot.com',
+    messagingSenderId: '935365070251',
+    appId: '1:935365070251:web:ebba9597ec722fb68816ee',
+  },
+};
