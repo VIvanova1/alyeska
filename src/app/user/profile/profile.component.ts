1c216dc7301de9d5f50ef4e2eba888f0960d006b
Veneta Ivanova <i.veneta.v@gmail.com>
2023-11-23 20:11:30 +0200

    change edit page

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { ActivatedRoute, Route, Router, RouterModule } from '@angular/router';
 import { EmployeeData } from 'src/app/model/user-data';
 import UserDataService from 'src/app/services/user-data.service';
 
@@ -7,46 +14,71 @@ import UserDataService from 'src/app/services/user-data.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
-  currentEmployee: EmployeeData | undefined;
+export class ProfileComponent implements OnInit, OnChanges, OnDestroy {
+  currentEmployee!: any;
   trainings: any[] = [];
-  id!: string;
-  moreInfo:any;
-  experienceInput:any;
-  schoolInput:any;
+  id: any;
+  moreInfo: any;
+  experienceInput: any;
+  schoolInput: any;
+  editId: any;
 
-  constructor(private userService: UserDataService) {
+  constructor(
+    private userService: UserDataService,
+    private router: ActivatedRoute
+  ) {
     this.experienceInput = {
       type: 'Experience',
       institution: 'Company',
       activity: 'Position',
     };
 
-    this.schoolInput ={
+    this.schoolInput = {
       type: 'Education',
       institution: 'School',
       activity: 'School major',
-    }
-   }
-
-  ngOnInit (): void {
-    const email = localStorage.getItem('user');
-    let user: any;
-    if (email) {
-      user = this.getUser(email);
-    }
+    };
   }
 
-  getUser(email: string) {
-    this.userService.findEmployee(email).subscribe((res) => {
-      res.map((user: any) => {
-        this.id = user.id;
-        this.id;
-        this.experienceInput.id=user.id
-        this.currentEmployee = user.data;
-        this.currentEmployee;
-      });
+  ngOnInit(): void {
+    this.router.queryParamMap.subscribe((params) => {
+      this.id = params.get('id');
+    });
+    this.fintUserForEdit(this.id);
+  }
+
+  fintUserForEdit(id: string) {
+    this.userService.getOneEmployee(id).subscribe((res) => {
+      this.currentEmployee = res;
     });
   }
 
+  ngOnDestroy(): void {
+    console.log('destroy');
+    this.currentEmployee = '';
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('change', changes);
+  }
+
+  // getUser(email: string) {
+  //   this.userService.findEmployee(email).subscribe((res) => {
+  //     res.map((user: any) => {
+  //       this.id = user.id;
+  //       this.id;
+  //       this.experienceInput.id = user.id;
+  //       this.currentEmployee = user.data;
+  //       this.currentEmployee;
+  //     });
+  //   });
+  // }
+
+  //in OnInit
+  // let user: any;
+  // const email = localStorage.getItem('user');
+  // if (email) {
+  //   user = this.getUser(email);
+  //   console.log('This is profile page');
+  // }
 }
